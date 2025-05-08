15d902d9939f3b5104973f857673ed34961be55b
pinki singh <web.pinkisingh@gmail.com>
2025-05-08 07:54:40 +0530

    ✨Refactor: today's leetcode question solution

--- may/lib/may.dart
+++ may/lib/may.dart
@@ -125,11 +125,14 @@ class ListNode {
 
 class Solution {
   ListNode? mergeTwoLists(ListNode? list1, ListNode? list2) {
+    if (list1 == null && list2 != null) return list2;
+    if (list2 == null && list1 != null) return list1;
+
     ListNode dummy = ListNode(0);
     ListNode current = dummy;
 
     while (list1 != null && list2 != null) {
-      if (list1.val < list2.val) {
+      if (list1.val <= list2.val) {
         current.next = list1;
         list1 = list1.next;
       } else {
